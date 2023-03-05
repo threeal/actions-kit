2209770395a0c7b9e9c3ff7520e3f7b08e94bb7f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-05 14:23:10 +0700

    add internal time object for calculating elapsed time in `log` package

---
+++ packages/log/src/internal/time.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, test } from "@jest/globals";
+import { Time } from "./time";
+
+describe("test getting current time", () => {
+  test("should be more than zero", () => {
+    expect(Time.now().ms).toBeGreaterThan(0);
+  });
+});
+
+describe("test calculate elapsed time", () => {
+  test("should be more than the timeout", async () => {
+    const time = Time.now();
+    await new Promise((r) => setTimeout(r, 50));
+    expect(time.elapsed().ms).toBeGreaterThan(50);
+  });
+  test("new one should be more than the old one", async () => {
+    const time = Time.now();
+    await new Promise((r) => setTimeout(r, 50));
+    const prevElapsed = time.elapsed();
+    await new Promise((r) => setTimeout(r, 50));
+    expect(time.elapsed().ms).toBeGreaterThan(prevElapsed.ms);
+  });
+});
