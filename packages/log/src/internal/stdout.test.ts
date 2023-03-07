6b3eff15cd54471b279835bf33a89d473e54f139
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-07 12:38:47 +0700

    `log` pkg add a test util function for executes Node code

--- packages/log/src/internal/stdout.test.ts
+++ packages/log/src/internal/stdout.test.ts
@@ -1,4 +1,5 @@
 import { beforeAll, describe, expect, test } from "@jest/globals";
+import { exec } from "child_process";
 
 export function stdout(): string {
   return process.env.TEST_STDOUT || "";
@@ -8,9 +9,31 @@ export function flushStdout() {
   process.env.TEST_STDOUT = "";
 }
 
+export async function nodeExec(code: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(`node -e "${code}"`, (error, out) => {
+      if (error) {
+        return reject(error);
+      }
+      process.stdout.write(`code:\n${code}\noutput:\n${out}\n`);
+      resolve(out);
+    });
+  });
+}
+
 describe("test flush stdout", () => {
   beforeAll(() => flushStdout());
   test("stdout should be empty", () => {
     expect(stdout()).toHaveLength(0);
+  });
+});
+
+describe("test node code execution", () => {
+  let prom: Promise<string>;
+  beforeAll(() => {
+    prom = nodeExec("console.log('some log');");
+  });
+  test("output should be correct", () => {
+    expect(prom).resolves.toBe("some log\n");
   });
 });
