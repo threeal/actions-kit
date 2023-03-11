6397b8135f145677b7becbcb4172ca8f700c47a4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-11 16:19:56 +0700

    remove `io` package

--- packages/io/lib/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.writeJsonFile = exports.readJsonFile = void 0;
-var json_1 = require("./json");
-Object.defineProperty(exports, "readJsonFile", { enumerable: true, get: function () { return json_1.readJsonFile; } });
-Object.defineProperty(exports, "writeJsonFile", { enumerable: true, get: function () { return json_1.writeJsonFile; } });
-//# sourceMappingURL=index.js.map
--- packages/io/lib/json.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="node" />
-import * as fs from "fs";
-export declare function writeJsonFile(file: fs.PathOrFileDescriptor, data: any): void;
-export declare function readJsonFile<Type>(file: fs.PathOrFileDescriptor): Type;
