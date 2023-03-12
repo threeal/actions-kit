7070610330725b18cb902e7fbde22f8d64ba2422
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-12 10:42:48 +0700

    Exec package, modifiy and expand `Result` object testing

--- packages/exec/src/result.test.ts
+++ packages/exec/src/result.test.ts
@@ -7,24 +7,45 @@ describe("constructs a new result object", () => {
     test("should not throws an error", () => {
       expect(() => (res = new Result(0))).not.toThrow();
     });
-    test("the status code should be defaulted to 0", () => {
-      expect(res.code).toBe(0);
-    });
-    test("the status should be ok", () => {
-      expect(res.isOk()).toBe(true);
+    describe("checks the properties", () => {
+      test("the status code should be defaulted to zero", () => {
+        expect(res.code).toBe(0);
+      });
+      test("the status should be ok", () => {
+        expect(res.isOk()).toBe(true);
+      });
     });
   });
 
   describe("with a status code specified", () => {
-    let res: Result;
-    test("should not throws an error", () => {
-      expect(() => (res = new Result(8))).not.toThrow();
+    describe("with a zero status code", () => {
+      let res: Result;
+      test("should not throws an error", () => {
+        expect(() => (res = new Result(0))).not.toThrow();
+      });
+      describe("checks the properties", () => {
+        test("the status code should be equals", () => {
+          expect(res.code).toBe(0);
+        });
+        test("the status should be ok", () => {
+          expect(res.isOk()).toBe(true);
+        });
+      });
     });
-    test("the status code should be equals", () => {
-      expect(res.code).toBe(8);
-    });
-    test("the status should not be ok", () => {
-      expect(res.isOk()).toBe(false);
+
+    describe("with a non zero status code", () => {
+      let res: Result;
+      test("should not throws an error", () => {
+        expect(() => (res = new Result(8))).not.toThrow();
+      });
+      describe("checks the properties", () => {
+        test("the status code should be equals", () => {
+          expect(res.code).toBe(8);
+        });
+        test("the status should not be ok", () => {
+          expect(res.isOk()).toBe(false);
+        });
+      });
     });
   });
 });
