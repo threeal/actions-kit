9cc21efe9de96808ac8d704b08f6c70d48f56059
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-13 20:24:29 +0700

    feat(exec): add `Command` class

--- packages/exec/lib/index.js
+++ packages/exec/lib/index.js
@@ -1,6 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Result = exports.execOut = exports.exec = void 0;
+exports.Result = exports.execOut = exports.exec = exports.Command = void 0;
+var command_1 = require("./command");
+Object.defineProperty(exports, "Command", { enumerable: true, get: function () { return command_1.Command; } });
 var exec_1 = require("./exec");
 Object.defineProperty(exports, "exec", { enumerable: true, get: function () { return exec_1.exec; } });
 Object.defineProperty(exports, "execOut", { enumerable: true, get: function () { return exec_1.execOut; } });
