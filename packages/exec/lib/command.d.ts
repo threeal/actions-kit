18304dcabadfcb4823e78b034676bda135847617
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-14 20:05:46 +0700

    feat(exec): add `Command.exec` method

--- packages/exec/lib/command.d.ts
+++ packages/exec/lib/command.d.ts
@@ -1,5 +1,7 @@
+import { Result } from "./result";
 export declare class Command {
     command: string;
     args: string[];
     constructor(command: string, ...args: string[]);
+    exec(...args: string[]): Promise<Result>;
 }
--- packages/exec/src/command.ts
+++ packages/exec/src/command.ts
@@ -1,3 +1,6 @@
+import { exec } from "./exec";
+import { Result } from "./result";
+
 export class Command {
   command: string;
   args: string[];
@@ -5,5 +8,9 @@ export class Command {
   constructor(command: string, ...args: string[]) {
     this.command = command;
     this.args = args;
+  }
+
+  async exec(...args: string[]): Promise<Result> {
+    return exec(this.command, ...this.args.concat(args));
   }
 }
