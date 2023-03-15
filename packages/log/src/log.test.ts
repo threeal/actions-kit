373e1a556b4d4fcc7fd803d42715dcde536eb945
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-15 13:55:03 +0700

    test(log): use `exec.Command.execOut` to replace `exec.execOut`

--- packages/log/src/log.test.ts
+++ packages/log/src/log.test.ts
@@ -2,6 +2,8 @@ import * as exec from "@actions-kit/exec";
 import { beforeAll, describe, expect, test } from "@jest/globals";
 import { error, fatal, warning } from "./log";
 
+const node = new exec.Command("node", "-e");
+
 describe("writes warning to log", () => {
   test("should not throw", () => {
     expect(() => warning("some message")).not.toThrow();
@@ -9,11 +11,10 @@ describe("writes warning to log", () => {
   describe("runs in a separate process", () => {
     let prom: Promise<exec.Result>;
     test("should be resolved", () => {
-      const code = [
-        "const log = require('./packages/log/lib');",
-        "log.warning('some message');",
-      ].join("\n");
-      prom = exec.execOut("node", "-e", code);
+      prom = node.execOut(
+        "const log = require('./packages/log/lib');\n\
+        log.warning('some message');"
+      );
       return expect(prom).resolves.toBeTruthy();
     });
     describe("checks output", () => {
@@ -36,11 +37,10 @@ describe("writes error to log", () => {
   describe("runs in a separate process", () => {
     let prom: Promise<exec.Result>;
     test("should be resolved", () => {
-      const code = [
-        "const log = require('./packages/log/lib');",
-        "log.error('some message');",
-      ].join("\n");
-      prom = exec.execOut("node", "-e", code);
+      prom = node.execOut(
+        "const log = require('./packages/log/lib');\n\
+        log.error('some message');"
+      );
       return expect(prom).resolves.toBeTruthy();
     });
     describe("checks output", () => {
@@ -63,11 +63,10 @@ describe("writes a fatal message to the log", () => {
   describe("runs in a separate process", () => {
     let prom: Promise<exec.Result>;
     test("should be resolved", () => {
-      const code = [
-        "const log = require('./packages/log/lib');",
-        "log.fatal('some message');",
-      ].join("\n");
-      prom = exec.execOut("node", "-e", code);
+      prom = node.execOut(
+        "const log = require('./packages/log/lib');\n\
+        log.fatal('some message');"
+      );
       return expect(prom).resolves.toBeTruthy();
     });
     describe("checks the output and the status", () => {
