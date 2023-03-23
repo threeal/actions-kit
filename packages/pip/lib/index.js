4bd24dcf622fc6a2af6f4bf5867687fe6c395f56
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-23 17:24:38 +0700

    feat(pip): add `install.ts` from `threeal/gcovr-action`

--- packages/pip/lib/index.js
+++ packages/pip/lib/index.js
@@ -1,6 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.pip = void 0;
+exports.uninstallPackage = exports.installPackage = exports.pip = void 0;
 var pip_1 = require("./pip");
 Object.defineProperty(exports, "pip", { enumerable: true, get: function () { return pip_1.pip; } });
+var install_1 = require("./install");
+Object.defineProperty(exports, "installPackage", { enumerable: true, get: function () { return install_1.installPackage; } });
+Object.defineProperty(exports, "uninstallPackage", { enumerable: true, get: function () { return install_1.uninstallPackage; } });
 //# sourceMappingURL=index.js.map
--- packages/pip/src/index.ts
+++ packages/pip/src/index.ts
@@ -1,1 +1,2 @@
 export { pip } from "./pip";
+export { installPackage, uninstallPackage } from "./install";
