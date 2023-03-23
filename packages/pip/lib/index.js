9ba2973759e74933ddbd0de8360174d56c9b8e78
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-23 17:13:04 +0700

    feat(pip): add `pip.ts` from `threeal/gcovr-action`

--- packages/pip/lib/index.js
+++ packages/pip/lib/index.js
@@ -1,2 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.pip = void 0;
+var pip_1 = require("./pip");
+Object.defineProperty(exports, "pip", { enumerable: true, get: function () { return pip_1.pip; } });
 //# sourceMappingURL=index.js.map
