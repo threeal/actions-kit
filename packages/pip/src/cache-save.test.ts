c8aba6055a0bed475e7700140d900e7e1768bde3
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-24 16:39:41 +0700

    test(pip): add `cache-save.test.ts` from `threeal/gcovr-action`

---
+++ packages/pip/src/cache-save.test.ts
@@ -0,0 +1,79 @@
+import { afterAll, beforeAll, describe, expect, test } from "@jest/globals";
+import * as fs from "fs";
+import { packageCacheInfoRemoveRoot, validPkgName } from "./cache.test";
+import { PackageCacheInfo, PackageContentCacheInfo } from "./cache";
+import { installPackage, uninstallPackage } from "./install";
+
+describe("test save cache of a pip package content info", () => {
+  describe(`using a valid package (${validPkgName})`, () => {
+    const cacheInfo = new PackageCacheInfo(validPkgName);
+    let contentInfo: PackageContentCacheInfo;
+    beforeAll(async () => {
+      await installPackage(cacheInfo.name);
+      contentInfo = await cacheInfo.accumulateContentInfo();
+      packageCacheInfoRemoveRoot();
+    });
+
+    describe("check the cache", () => {
+      test("content info file should not be exist", () => {
+        expect(fs.existsSync(cacheInfo.path)).toBe(false);
+      });
+    });
+
+    describe("save the cache", () => {
+      test("should be resolved", async () => {
+        const prom = cacheInfo.saveContentInfo(contentInfo);
+        await expect(prom).resolves.toBeUndefined();
+      });
+    });
+
+    describe("check again the cache", () => {
+      test("content info file should be exist", () => {
+        expect(fs.existsSync(cacheInfo.path)).toBe(true);
+      });
+    });
+
+    afterAll(async () => {
+      await uninstallPackage(cacheInfo.name);
+      packageCacheInfoRemoveRoot();
+    });
+  });
+});
+
+describe("test save cache of a pip package content", () => {
+  describe(`using a valid package (${validPkgName})`, () => {
+    const cacheInfo = new PackageCacheInfo(validPkgName);
+    let contentInfo: PackageContentCacheInfo;
+    beforeAll(async () => {
+      await installPackage(cacheInfo.name);
+      contentInfo = await cacheInfo.accumulateContentInfo();
+      await cacheInfo.saveContentInfo(contentInfo);
+    });
+
+    describe("check the package", () => {
+      test("files should be exist", () => {
+        for (const path of contentInfo.paths) {
+          expect(fs.existsSync(path)).toBe(true);
+        }
+      });
+    });
+
+    describe("save the cache", () => {
+      test("should be resolved", async () => {
+        const prom = contentInfo.save();
+        await expect(prom).resolves.toBeUndefined();
+      });
+    });
+
+    describe("check the cache", () => {
+      test("should be saved", async () => {
+        const prom = contentInfo.restore();
+        await expect(prom).resolves.not.toBeUndefined();
+      });
+    });
+
+    afterAll(async () => {
+      await uninstallPackage(cacheInfo.name);
+    });
+  });
+});
