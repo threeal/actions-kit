5e53d4f6368c9cad53ca9b6b8e0d74783c9ced83
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-03-25 16:56:36 +0700

    test(exec): merge executes a command tests to reuse the same flow

--- packages/exec/src/exec.test.ts
+++ packages/exec/src/exec.test.ts
@@ -2,60 +2,54 @@ import { beforeAll, describe, expect, test } from "@jest/globals";
 import { exec, execOut, execSilently } from "./exec";
 import { Result } from "./result";
 
-describe("executes a command", () => {
-  describe("on a successful command", () => {
-    let prom: Promise<Result>;
-    test("should be resolved", () => {
-      prom = exec("node", "-e", "process.exit();");
-      return expect(prom).resolves.toBeTruthy();
-    });
-    describe("checks the result", () => {
-      let res: Result;
-      beforeAll(async () => (res = await prom));
-      test("the status should be ok", () => expect(res.isOk()).toBe(true));
-    });
-  });
+interface TestExecParams {
+  title: string;
+  successExec: () => Promise<Result>;
+  failExec: () => Promise<Result>;
+}
 
-  describe("on a failed command", () => {
-    let prom: Promise<Result>;
-    test("should be resolved", () => {
-      prom = exec("node", "-e", "process.exit(1)");
-      return expect(prom).resolves.toBeTruthy();
+function testExec(params: TestExecParams) {
+  describe(params.title, () => {
+    describe("on a successful command", () => {
+      let prom: Promise<Result>;
+      test("should be resolved", () => {
+        prom = params.successExec();
+        return expect(prom).resolves.toBeTruthy();
+      });
+      describe("checks the result", () => {
+        let res: Result;
+        beforeAll(async () => (res = await prom));
+        test("the status should be ok", () => expect(res.isOk()).toBe(true));
+      });
     });
-    describe("checks the result", () => {
-      let res: Result;
-      beforeAll(async () => (res = await prom));
-      test("the status should not be ok", () => expect(res.isOk()).toBe(false));
+
+    describe("on a failed command", () => {
+      let prom: Promise<Result>;
+      test("should be resolved", () => {
+        prom = params.failExec();
+        return expect(prom).resolves.toBeTruthy();
+      });
+      describe("checks the result", () => {
+        let res: Result;
+        beforeAll(async () => (res = await prom));
+        test("the status should not be ok", () => {
+          expect(res.isOk()).toBe(false);
+        });
+      });
     });
   });
+}
+
+testExec({
+  title: "executes a command",
+  successExec: () => exec("node", "-e", "process.exit();"),
+  failExec: () => exec("node", "-e", "process.exit(1)"),
 });
 
-describe("executes a command silently", () => {
-  describe("on a successful command", () => {
-    let prom: Promise<Result>;
-    test("should be resolved", () => {
-      prom = execSilently("node", "-e", "process.exit();");
-      return expect(prom).resolves.toBeTruthy();
-    });
-    describe("checks the result", () => {
-      let res: Result;
-      beforeAll(async () => (res = await prom));
-      test("the status should be ok", () => expect(res.isOk()).toBe(true));
-    });
-  });
-
-  describe("on a failed command", () => {
-    let prom: Promise<Result>;
-    test("should be resolved", () => {
-      prom = execSilently("node", "-e", "process.exit(1)");
-      return expect(prom).resolves.toBeTruthy();
-    });
-    describe("checks the result", () => {
-      let res: Result;
-      beforeAll(async () => (res = await prom));
-      test("the status should not be ok", () => expect(res.isOk()).toBe(false));
-    });
-  });
+testExec({
+  title: "executes a command silently",
+  successExec: () => execSilently("node", "-e", "process.exit();"),
+  failExec: () => execSilently("node", "-e", "process.exit(1)"),
 });
 
 describe("executes a command and gets the output", () => {
