81413447d1effac6b4b1b827f8bd145b62270c28
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-04-13 21:22:23 +0700

    build(pip): make Prettier write correct format on most files

--- packages/pip/jest.config.ts
+++ packages/pip/jest.config.ts
@@ -1,7 +1,7 @@
 import config from "@actions-kit/dev-jest";
 
 config.moduleNameMapper = {
-  'actions/cache': '@actions-kit/mock-actions-cache/lib/index.js',
+  "actions/cache": "@actions-kit/mock-actions-cache/lib/index.js",
 };
 
 export default config;
