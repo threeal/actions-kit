14a8af552c0e824040bf5cae12957915c927fb7f
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-04-15 19:41:58 +0700

    test: specify run in band option of Jest to be enabled from the config instead

--- internal/dev/src/jest.ts
+++ internal/dev/src/jest.ts
@@ -14,6 +14,7 @@ export function jestConfig(config?: Config): Config {
       },
     },
     moduleFileExtensions: ["js", "ts"],
+    runner: "jest-serial-runner",
     testEnvironment: "node",
     testMatch: ["**/*.test.ts"],
     transform: {
