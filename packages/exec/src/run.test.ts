7643825a35ca16e7a2cfc28f319e0933ec69ef8e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-04-28 17:12:21 +0700

    refactor(exec): simplify test for constructing a new command run result

--- packages/exec/src/run.test.ts
+++ packages/exec/src/run.test.ts
@@ -3,34 +3,16 @@ import { newHook, testCheckRunResult, testOutputSilent } from "./helper.test";
 import { run, RunResult, runSilently } from "./run";
 
 describe("constructs a new command run result", () => {
-  describe("with a zero status code", () => {
-    let res: RunResult;
-    test("should not throws an error", () => {
-      expect(() => (res = new RunResult(0))).not.toThrow();
-    });
-    describe("checks the properties", () => {
-      test("the status code should be equals", () => {
-        expect(res.code).toBe(0);
-      });
-      test("the status should be ok", () => {
-        expect(res.isOk()).toBe(true);
-      });
-    });
+  test("with a zero status code", () => {
+    const res = new RunResult(0);
+    expect(res.code).toBe(0);
+    expect(res.isOk()).toBe(true);
   });
 
-  describe("with a non zero status code", () => {
-    let res: RunResult;
-    test("should not throws an error", () => {
-      expect(() => (res = new RunResult(8))).not.toThrow();
-    });
-    describe("checks the properties", () => {
-      test("the status code should be equals", () => {
-        expect(res.code).toBe(8);
-      });
-      test("the status should not be ok", () => {
-        expect(res.isOk()).toBe(false);
-      });
-    });
+  test("with a non zero status code", () => {
+    const res = new RunResult(8);
+    expect(res.code).toBe(8);
+    expect(res.isOk()).toBe(false);
   });
 });
 
