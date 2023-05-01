f6fdf76311ae3c965ba9d98739f739f787f59af0
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-01 22:30:02 +0700

    test(exec): use argument to decide whether the mocked exec function exit with success or not

--- packages/exec/src/run.test.ts
+++ packages/exec/src/run.test.ts
@@ -10,22 +10,23 @@ mocked.exec.mockImplementation(async (commandLine, args, options) => {
   expect(commandLine).toBe("test");
   if (args === undefined) throw new Error("args should not be undefined");
   if (options === undefined) throw new Error("options should not be undefined");
+  let code = 0;
   for (const arg of args) {
     switch (arg) {
-      case "--no-silent": {
+      case "--no-silent":
         expect(options.silent).toBe(false);
         break;
-      }
-      case "--silent": {
+      case "--silent":
         expect(options.silent).toBe(true);
         break;
-      }
-      default: {
-        return parseInt(arg);
-      }
+      case "--fail":
+        code = 1;
+        break;
+      default:
+        throw new Error(`unknown argument: ${arg}`);
     }
   }
-  throw new Error("args should contains an exit code");
+  return code;
 });
 
 describe("constructs a new command run result", () => {
@@ -44,24 +45,24 @@ describe("constructs a new command run result", () => {
 
 describe("runs a command", () => {
   test("on a successful command", async () => {
-    const res = await run("test", "--no-silent", "0");
+    const res = await run("test", "--no-silent");
     expect(res.isOk()).toBe(true);
   });
 
   test("on a failed command", async () => {
-    const res = await run("test", "--no-silent", "1");
+    const res = await run("test", "--no-silent", "--fail");
     expect(res.isOk()).toBe(false);
   });
 });
 
 describe("runs a command silently", () => {
   test("on a successful command", async () => {
-    const res = await runSilently("test", "--silent", "0");
+    const res = await runSilently("test", "--silent");
     expect(res.isOk()).toBe(true);
   });
 
   test("on a failed command", async () => {
-    const res = await runSilently("test", "--silent", "1");
+    const res = await runSilently("test", "--silent", "--fail");
     expect(res.isOk()).toBe(false);
   });
 });
