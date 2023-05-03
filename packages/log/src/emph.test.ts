9d2bbb8e5a14629c441279573fecacd6ec5b1c5e
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-03 21:22:34 +0700

    test(log): simplify and modify regex match in the `emphasizes message` test

--- packages/log/src/emph.test.ts
+++ packages/log/src/emph.test.ts
@@ -1,8 +1,6 @@
-import { describe, expect, test } from "@jest/globals";
+import { expect, test } from "@jest/globals";
 import { emph } from "./emph";
 
-describe("emphasizes message", () => {
-  test("message should be written", () => {
-    expect(emph("some message")).toMatch(/some message/);
-  });
+test("emphasizes message", () => {
+  expect(emph("some message")).toMatch(/.+some message.+/);
 });
