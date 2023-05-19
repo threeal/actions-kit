7a0ec5e63020a8825040c16b36bf54c54c9829c6
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-19 17:08:10 +0700

    feat(dev): add a `build` command in the `dev` binary

--- packages/dev/lib/dev.js
+++ packages/dev/lib/dev.js
@@ -1,7 +1,14 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const child_process_1 = require("child_process");
 const commander_1 = require("commander");
 const program = new commander_1.Command();
+program
+    .command("build")
+    .description("compile source files from TypeScript to JavaScript")
+    .action(() => {
+    (0, child_process_1.spawnSync)("tsc", [], { stdio: "inherit" });
+});
 program.parse();
 //# sourceMappingURL=dev.js.map
--- packages/dev/src/dev.ts
+++ packages/dev/src/dev.ts
@@ -1,7 +1,15 @@
 #!/usr/bin/env node
 
+import { spawnSync } from "child_process";
 import { Command } from "commander";
 
 const program = new Command();
+
+program
+  .command("build")
+  .description("compile source files from TypeScript to JavaScript")
+  .action(() => {
+    spawnSync("tsc", [], { stdio: "inherit" });
+  });
 
 program.parse();
