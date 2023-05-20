9439da4383c6ab10d9bb3edc028c5500c91163eb
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-20 22:12:44 +0700

    test(i-test): add an integration test for uninstalling a pip package

--- internal/integration-test/src/pip.test.ts
+++ internal/integration-test/src/pip.test.ts
@@ -21,5 +21,22 @@ describe("installs and uninstalls a pip package (rsa)", () => {
     });
   });
 
+  describe("uninstalls the rsa package", () => {
+    test("the package info should not be undefined", () => {
+      const prom = pip.showPackageInfo("rsa");
+      return expect(prom).resolves.not.toBeUndefined();
+    });
+
+    test("uninstalls the package", () => {
+      const prom = pip.uninstallPackage("rsa");
+      return expect(prom).resolves.toBeUndefined();
+    });
+
+    test("uninstalls the package again", () => {
+      const prom = pip.uninstallPackage("rsa");
+      return expect(prom).resolves.toBeUndefined();
+    });
+  });
+
   afterAll(() => pip.uninstallPackage("rsa"));
 })
