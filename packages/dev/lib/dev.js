9aebc30d087e8e133540e662f50ce05ead9b74ea
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-05-20 20:14:30 +0700

    refactor(dev): simplify command execution using a function

--- packages/dev/lib/dev.js
+++ packages/dev/lib/dev.js
@@ -3,26 +3,21 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const child_process_1 = require("child_process");
 const commander_1 = require("commander");
+function exec(command, ...args) {
+    (0, child_process_1.spawnSync)(command, args, { stdio: "inherit" });
+}
 const program = new commander_1.Command();
 program
     .command("build")
     .description("compile source files from TypeScript to JavaScript")
-    .action(() => {
-    (0, child_process_1.spawnSync)("tsc", [], { stdio: "inherit" });
-});
+    .action(() => exec("tsc"));
 program
     .command("clean")
     .description("clean the build output directory")
-    .action(() => {
-    (0, child_process_1.spawnSync)("rimraf", ["lib"], { stdio: "inherit" });
-});
+    .action(() => exec("rimraf", "lib"));
 program
     .command("format")
     .description("format the source files using Prettier")
-    .action(() => {
-    (0, child_process_1.spawnSync)("prettier", ["--write", "*.{js,ts,json}", "src/**/*.ts"], {
-        stdio: "inherit",
-    });
-});
+    .action(() => exec("prettier", "--write", "*.{js,ts,json}", "src/**/*.ts"));
 program.parse();
 //# sourceMappingURL=dev.js.map
--- packages/dev/src/dev.ts
+++ packages/dev/src/dev.ts
@@ -3,29 +3,25 @@
 import { spawnSync } from "child_process";
 import { Command } from "commander";
 
+function exec(command: string, ...args: string[]) {
+  spawnSync(command, args, { stdio: "inherit" });
+}
+
 const program = new Command();
 
 program
   .command("build")
   .description("compile source files from TypeScript to JavaScript")
-  .action(() => {
-    spawnSync("tsc", [], { stdio: "inherit" });
-  });
+  .action(() => exec("tsc"));
 
 program
   .command("clean")
   .description("clean the build output directory")
-  .action(() => {
-    spawnSync("rimraf", ["lib"], { stdio: "inherit" });
-  });
+  .action(() => exec("rimraf", "lib"));
 
 program
   .command("format")
   .description("format the source files using Prettier")
-  .action(() => {
-    spawnSync("prettier", ["--write", "*.{js,ts,json}", "src/**/*.ts"], {
-      stdio: "inherit",
-    });
-  });
+  .action(() => exec("prettier", "--write", "*.{js,ts,json}", "src/**/*.ts"));
 
 program.parse();
