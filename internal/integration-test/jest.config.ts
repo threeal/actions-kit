05e3a4547ef3afe4ae5debe8908e3f6c4a9e7aa4
Alfi Maulana <alfi.maulana.f@gmail.com>
2023-11-23 22:11:17 +0700

    feat: remove internal packages

--- internal/integration-test/jest.config.ts
+++
@@ -1,7 +0,0 @@
-import { createJestConfig } from "@actions-kit/dev";
-
-export default createJestConfig((config) => {
-  config.collectCoverage = false;
-  config.testMatch?.push("!**/helper.test.ts");
-  return config;
-});
